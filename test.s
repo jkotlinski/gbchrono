c58547fb29b414dc9574584c63f8e96dd631c5f9
Johan Kotlinski <kotlinski@gmail.com>
2023-03-14 21:35:34 +0100

    tidy up

--- test.s
+++ test.s
@@ -2,10 +2,10 @@
 ; Copyright (c) 2023 Johan Kotlinski
 
 SECTION "timer",ROM0[$50]
-      jp    timer
+      jp    timer_isr
 
 SECTION "joypad",ROM0[$60]
-      jp    joypad
+      jp    joypad_isr
 
 SECTION "boot",ROM0[$100]
       jr    main
@@ -15,14 +15,14 @@ digits:
 
 SECTION "test",ROM0[$150]
 main:
-      ; disable screen
+      ; disable screen {{{
 :     ldh   a,[$44]     ; LY
       cp    a,144
       jr    nz,:-
       xor   a
       ldh   [$40],a     ; LCDC
-
-      ; initialize RAM
+      ; }}}
+      ; initialize RAM {{{
       xor   a
       ld    hl,digits
       ld    [hl+],a
@@ -33,24 +33,44 @@ main:
       ld    [hl+],a
       ld    [hl+],a
       ld    [hl+],a
-
-      ; start 4096/4=1024 Hz timer
-      ld    a,-4
+      ; }}}
+      ; wait for start press {{{
+      ; select P15 by setting it low
+      ld    a,$10
+      ldh   [0],a
+      ; wait for button down
+:     ldh   a,[0]
+      and   a,$f
+      cp    a,$f
+      jr    z,:-
+      ; wait for button up
+:     ldh   a,[0]
+      and   a,$f
+      cp    a,$f
+      jr    nz,:-
+      ; }}}
+      ; enable timer + interrupts {{{
+      ; start 4096/16=256 Hz timer
+      ld    a,-16
       ldh   [6],a ; TMA
       ld    a,4
       ldh   [7],a ; TAC
 
       ; enable timer and joypad interrupts
+      xor   a
+      ldh   [$f],a      ; IF
       ld    a,$14
-      ldh   [$ff],a
+      ldh   [$ff],a     ; IE
 
       ei
-
+      ; }}}
+      ; idle loop {{{
 :     halt
       nop
       jr    :-
+      ; }}}
 
-timer:
+timer_isr: ; {{{
       ld    c,low(digits)
 :     ldh   a,[c]
       inc   a
@@ -62,5 +82,17 @@ timer:
       jr    :-
 :     ldh   [c],a
       reti
+      ; }}}
 
-joypad:
+joypad_isr: ; {{{
+      ; wait for button up
+:     ldh   a,[0]
+      and   a,$f
+      cp    a,$f
+      jr    nz,:-
+      ; re-enable screen
+      ld    a,$91
+      ldh   [$40],a     ; LCDC
+      halt
+      nop
+      ; }}}
