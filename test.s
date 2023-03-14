0d41915a766dfdd231f6954ae8bce68191c97fce
Johan Kotlinski <kotlinski@gmail.com>
2023-03-14 20:44:43 +0100

    tidy up

--- test.s
+++ test.s
@@ -2,62 +2,65 @@
 ; Copyright (c) 2023 Johan Kotlinski
 
 SECTION "timer",ROM0[$50]
-	jp	timer
+      jp    timer
 
 SECTION "joypad",ROM0[$60]
-	jp	joypad
+      jp    joypad
 
 SECTION "boot",ROM0[$100]
-        jr      main
+      jr    main
 
 SECTION "hram",HRAM[$ff80]
 digits:
 
 SECTION "test",ROM0[$150]
 main:
-	di
+      ; disable screen
+:     ldh   a,[$44]     ; LY
+      cp    a,144
+      jr    nz,:-
+      xor   a
+      ldh   [$40],a     ; LCDC
 
-	; disable screen
-: 	ldh	a,[$44]	; LY
-	cp	a,144
-	jr	nz,:-
-	xor	a
-	ldh	[$40],a	; LCDC
+      ; initialize RAM
+      xor   a
+      ld    hl,digits
+      ld    [hl+],a
+      ld    [hl+],a
+      ld    [hl+],a
+      ld    [hl+],a
+      ld    [hl+],a
+      ld    [hl+],a
+      ld    [hl+],a
+      ld    [hl+],a
 
-	xor	a
-	ld	hl,digits
-	ld	[hl+],a
-	ld	[hl+],a
-	ld	[hl+],a
-	ld	[hl+],a
-	ld	[hl+],a
-	ld	[hl+],a
-	ld	[hl+],a
-	ld	[hl+],a
+      ; start 4096/4=1024 Hz timer
+      ld    a,-4
+      ldh   [6],a ; TMA
+      ld    a,4
+      ldh   [7],a ; TAC
 
-	; start 4096/256=16 Hz timer
-	ldh	[6],a	; TMA
-	ld	a,4
-	ldh	[7],a	; TAC
+      ; enable timer and joypad interrupts
+      ld    a,$14
+      ldh   [$ff],a
 
-	; enable timer and joypad interrupts
-	ld	a,$14
-	ldh	[$ff],a
+      ei
 
-	ei
+:     halt
+      nop
+      jr    :-
 
-: 	halt
-	nop
-	jr 	:-
-
-timer:  ld	l,low(digits)
-:  	inc	[hl]
-	ld	a,[hl]
-	cp	a,10
-	jr	nz,:+
-	ld	[hl],0
-	inc	l
-	jr	:-
-: 	reti
+timer:
+      ld    c,low(digits)
+:     ldh   a,[c]
+      inc   a
+      cp    a,10
+      jr    nz,:+
+      xor   a
+      ldh   [c],a
+      inc   c
+      jr    :-
+:     ldh   [c],a
+      reti
 
 joypad:
